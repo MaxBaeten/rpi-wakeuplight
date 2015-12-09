ed6441a215811d9106d3b0439b7e360f79786068
Max Baeten <m.j.j.baeten@student.tue.nl>
2015-12-09 07:14:23 +0100

    Started on python wakeuplight

---
+++ wekker.py
@@ -0,0 +1,51 @@
+import schedule
+import time
+
+# Constants
+wakeup_time_start = "5.10"
+wakeup_time_alarm = "5.40"
+
+# Functions
+def alarm_on():
+    print("BEEP BEEP BEEP WAKEUP!")
+
+def set_light_intensity(int intensity):
+    print("Setting the light intensity to:!"+intensity)
+
+def wakeup_
+	schedule.every().day.at(wakeup_time_start).do(job)
+
+def job():
+    print("I'm working...")
+
+# State machine
+while idTrue:
+    schedule.every().day.at(wakeup_time_start).do(job)
+    schedule.run_pending()
+    time.sleep(1)
+
+
+
+
+
+#	State machine
+#
+#	State A: Idle 
+#
+#
+#	State B: Active alarm
+#
+#			B1: Waiting for Sunrise
+#			B2: Sunrise
+#			B3: Wakeup
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
